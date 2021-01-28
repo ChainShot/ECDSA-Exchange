12b82cb3db4c5bd8b4c5989df08345b4079ba5b8
RJL <rjl2155@columbia.edu>
2021-01-28 18:59:44 -0500

    updated client requests and added UI

--- client/index.js
+++ client/index.js
@@ -1,3 +1,7 @@
+const EC = require("elliptic").ec;
+const SHA256 = require("crypto-js/sha256");
+const ec = new EC("secp256k1");
+
 import "./index.scss";
 
 const server = "http://localhost:3042";
@@ -9,8 +13,9 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
   }
 
   fetch(`${server}/balance/${value}`).then((response) => {
-    return response.json();
-  }).then(({ balance }) => {
+      return response.json();
+    })
+  .then(({ balance }) => {
     document.getElementById("balance").innerHTML = balance;
   });
 });
@@ -19,14 +24,32 @@ document.getElementById("transfer-amount").addEventListener('click', () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const yourKey = document.getElementById("private-key").value;
+
+
+  // set up the signing with the private key
+  const key = ec.keyFromPrivate(yourKey.toString(16));
+  const msg = "This is me sending you some fake money";
+  const msgHash = SHA256(msg);
+  const signature = key.sign(msgHash.toString());
+
+  const payload = {
+    msg,
+    signature: {
+      r: signature.r.toString(16),
+      s: signature.s.toString(16),
+    },
+  }
+
+  console.log(payload);
 
   const body = JSON.stringify({
-    sender, amount, recipient
+    sender, amount, recipient, payload
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
 
-  fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
+  fetch(request, { headers: { "Content-Type": "application/json" }}).then(response => {
     return response.json();
   }).then(({ balance }) => {
     document.getElementById("balance").innerHTML = balance;
--- server/index.js
+++ server/index.js
@@ -2,31 +2,86 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
+const EC = require("elliptic").ec;
+const { SHA256 } = require("crypto-js");
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
+// Create and initialize EC context
+// (better do it once and reuse it)
+var ec = new EC("secp256k1");
+
+// Generate keys
+const key1 = ec.genKeyPair();
+const key2 = ec.genKeyPair();
+const key3 = ec.genKeyPair();
+
+// public keys
+const pubKey1 = key1.getPublic().encode("hex");
+const pubKey2 = key2.getPublic().encode("hex");
+const pubKey3 = key3.getPublic().encode("hex");
+
+// private keys
+const privKey1 = key1.getPrivate().toString(16);
+const privKey2 = key2.getPrivate().toString(16);
+const privKey3 = key3.getPrivate().toString(16);
+
 const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
-}
+  [pubKey1]: 100,
+  [pubKey2]: 100,
+  [pubKey3]: 567,
+};
 
 app.get('/balance/:address', (req, res) => {
-  const {address} = req.params;
+  const { address } = req.params;
   const balance = balances[address] || 0;
   res.send({ balance });
 });
 
-app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+app.post("/send", (req, res) => {
+  const {sender, recipient, amount, payload} = req.body;
+  const receivedPubKey = sender;
+  const decodedKey = ec.keyFromPublic(receivedPubKey, "hex");
+  const msg = payload.msg;
+  const msgHash = SHA256(msg).toString();
+  const signature = {
+    r: payload.signature.r,
+    s: payload.signature.s,
+  };
+
+  if (decodedKey.verify(msgHash, signature)){
+    console.log(`Great job! You put in the correct key, and you sent ${amount}`)
+    balances[sender] -= amount;
+    balances[recipient] = (balances[recipient] || 0) + +amount;
+    res.send({ balance: balances[sender] });
+
+  }
+  else{
+    console.log("error, your private key isn't verified!");
+  }
+
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
+  console.log(
+    `
+    =====================
+    Public Address (and balances)
+    =====================
+
+    ${JSON.stringify(balances, null, 2)}
+    =====================
+    Private Keys
+    =====================
+    (0) ${privKey1}
+    (1) ${privKey2}
+    (2) ${privKey3}
+    `
+  );
+
+
 });
