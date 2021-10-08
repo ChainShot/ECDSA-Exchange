49fdda8ab72b55b496ebd9fd2a16abb327db07e1
jordang7 <77jgreenberg77@gmail.com>
2021-10-08 16:43:50 -0400

    completed both challenges

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
 import "./index.scss";
-
+var EC = require('elliptic').ec;
+var ec = new EC('secp256k1');
+const SHA256 = require('crypto-js/sha256');
 const server = "http://localhost:3042";
 
 document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
@@ -11,6 +13,8 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
   fetch(`${server}/balance/${value}`).then((response) => {
     return response.json();
   }).then(({ balance }) => {
+    document.getElementById("errorMsg").style.display = 'none';
+    document.getElementById("balance").style.display = 'block';
     document.getElementById("balance").innerHTML = balance;
   });
 });
@@ -19,16 +23,38 @@ document.getElementById("transfer-amount").addEventListener('click', () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const privateKey = document.getElementById("private-key").value;
 
+  const key = ec.keyFromPrivate(privateKey);
+
+  const message = {
+    sender:sender,
+    amount:amount,
+    recipient:recipient,
+    key:key
+  }
+
+  const msgHash = SHA256(message).toString();
+
+  const signature = key.sign(msgHash)
   const body = JSON.stringify({
-    sender, amount, recipient
+    sender, amount, recipient,signature, msgHash
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
 
   fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
     return response.json();
-  }).then(({ balance }) => {
-    document.getElementById("balance").innerHTML = balance;
+  }).then(({ balance,error }) => {
+    if(error){
+      document.getElementById("errorMsg").style.display = 'block';
+      document.getElementById("balance").style.display = 'none';
+      document.getElementById("errorMsg").innerHTML = error;
+    }else{
+      document.getElementById("balance").style.display = 'block';
+      document.getElementById("errorMsg").style.display = 'none';
+      document.getElementById("balance").innerHTML = balance;
+    }
+
   });
 });
--- server/index.js
+++ server/index.js
@@ -3,15 +3,41 @@ const app = express();
 const cors = require('cors');
 const port = 3042;
 
+var EC = require('elliptic').ec;
+var ec = new EC('secp256k1');
+
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+
+// const balances = {
+//   "1": 100,
+//   "2": 50,
+//   "3": 75,
+// }
+let balances ={};
+let privateKeys ={}
+
+let count =0
+while (count < 3){
+    const key = ec.genKeyPair();
+    const publicKey = key.getPublic().encode('hex');
+    balances[publicKey] =  Math.floor(Math.random() * 100);
+    const privateKey = key.getPrivate().toString(16)
+    privateKeys[publicKey] = privateKey
+    count++
+}
+console.log("Available Accounts")
+console.log("===================")
+for (let pub_key in balances){
+  console.log( "-" + pub_key + " (" + balances[pub_key] + ")")
+}
+console.log("Private Keys")
+console.log("===================")
+for (pub_key in balances){
+  console.log( "-" + privateKeys[pub_key])
 }
 
 app.get('/balance/:address', (req, res) => {
@@ -21,10 +47,21 @@ app.get('/balance/:address', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+  const {sender, amount, recipient,signature, msgHash} = req.body;
+  try{
+    const key = ec.keyFromPublic(sender,'hex');
+    if(key.verify(msgHash, signature)){
+      balances[sender] -= amount;
+      balances[recipient] = (balances[recipient] || 0) + +amount;
+      res.send({ balance: balances[sender] });
+    } else{
+      res.send({error: "Incorrect Private Key!"})
+    }
+  } catch(e){
+    res.send({error: "Invalid address"})
+  }
+
+
 });
 
 app.listen(port, () => {
