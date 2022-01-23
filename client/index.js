3cd92063512f80da67fe9958d27457ea1e309d0c
0xhaz <hazrienne@gmail.com>
2022-01-24 02:12:38 +0800

    Week 1 Exchange Project

--- client/index.js
+++ client/index.js
@@ -1,3 +1,7 @@
+const EC = require("elliptic").ec;
+const SHA256 = require("crypto-js/sha256");
+const ec = new EC("secp256k1");
+
 import "./index.scss";
 
 const server = "http://localhost:3042";
@@ -9,8 +13,9 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
   }
 
   fetch(`${server}/balance/${value}`).then((response) => {
-    return response.json();
-  }).then(({ balance }) => {
+      return response.json();
+    })
+  .then(({ balance }) => {
     document.getElementById("balance").innerHTML = balance;
   });
 });
@@ -19,14 +24,33 @@ document.getElementById("transfer-amount").addEventListener('click', () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const password = document.getElementById("private-key").value;
+  
+
+
+  // set up signing with the private key
+  const key = ec.keyFromPrivate(password.toString(16));
+  const msg = "Signing Message From Sender";
+  const msgHash = SHA256(msg);
+  const signature = key.sign(msgHash.toString());
+
+  const transaction = {
+    msg,
+    signature: {
+      r: signature.r.toString(16),
+      s: signature.s.toString(16),
+    }
+  }
+
+  console.log(transaction);
 
   const body = JSON.stringify({
-    sender, amount, recipient
+    sender, amount, recipient, transaction
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
 
-  fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
+  fetch(request, { headers: { "Content-Type": "application/json" }}).then(response => {
     return response.json();
   }).then(({ balance }) => {
     document.getElementById("balance").innerHTML = balance;
