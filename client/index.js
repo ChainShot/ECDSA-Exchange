8f51373c154e1ebe4753938aa4b377dfb9089934
Eric Le <ericle@Erics-MacBook-Pro.local>
2022-04-30 02:09:47 -0700

    finish project

--- client/index.js
+++ client/index.js
@@ -1,4 +1,6 @@
 import "./index.scss";
+const SHA256 = require('crypto-js/sha256');
+const secp = require("@noble/secp256k1");
 
 const server = "http://localhost:3042";
 
@@ -15,13 +17,21 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
   });
 });
 
-document.getElementById("transfer-amount").addEventListener('click', () => {
+document.getElementById("transfer-amount").addEventListener('click', async () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const privateKey = document.getElementById("private-key").value;
+
+  const msgHash = SHA256(JSON.stringify({
+    sender, amount, recipient
+  }));
+
+  let signature = await secp.sign(msgHash.toString(), privateKey);
+  signature = Buffer.from(signature).toString("hex");
 
   const body = JSON.stringify({
-    sender, amount, recipient
+    sender, amount, recipient, privateKey, signature
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
